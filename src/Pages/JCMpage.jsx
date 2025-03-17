4766fc014c16a0be4481c752b30ab7c0ba77d099
vincy-vince <vincyvince510@gmail.com>
2025-03-17 09:23:04 +0530

    few sessions added

--- src/Pages/JCMpage.jsx
+++ src/Pages/JCMpage.jsx
@@ -5,7 +5,57 @@ const JCMpage = () => {
   return (
     <div>
       <MenuBar />
-      <div className="jcm-container"></div>
+      <div className="jcm-container">
+        <h1>Judea Christ Ministries</h1>
+        <button>Register Now</button>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni
+        nulla harum nesciunt a nihil laborum eum ipsam quasi fugiat. Voluptatum,
+        nam. Ratione, ullam quam animi dolorum modi est accusamus! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Unde magni nulla harum
+        nesciunt a nihil laborum eum ipsam quasi fugiat. Voluptatum, nam.
+        Ratione, ullam quam animi dolorum modi est accusamus! Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Unde magni nulla harum nesciunt a
+        nihil laborum eum ipsam quasi fugiat. Voluptatum, nam. Ratione, ullam
+        quam animi dolorum modi est accusamus!
+      </p>
+      <div className="jcm-sec-2">
+        <img src="public/sec-2.png" alt="" />
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro
+          perferendis quasi eius, distinctio totam vitae fugit suscipit
+          accusantium. Illum esse voluptas rem excepturi enim ducimus voluptates
+          voluptate voluptatum, quasi cumque eos, quo recusandae temporibus
+          tenetur adipisci culpa doloremque ipsam aut odit distinctio. Maxime,
+          nemo ea! Temporibus non ipsa provident nobis.
+          <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
+          quasi illum fuga delectus, dignissimos vitae rerum, assumenda in ipsa
+          quibusdam iste asperiores ex fugiat suscipit eaque, provident nobis
+          cumque quia eum dolore magnam laboriosam perferendis. Voluptas dolores
+          voluptatum odio dolorum consectetur, rem voluptates iste saepe magni
+          est corrupti alias qui at nulla, mollitia quisquam totam aliquid ab
+          ipsam, quos dolorem?
+        </div>
+      </div>
+      <div className="jcm-sec-3">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia,
+          fugiat possimus fuga delectus ut tempora, minus nobis dolorem est
+          cumque repudiandae, numquam iure veritatis provident officiis
+          assumenda tempore quae eaque? Molestias nisi asperiores dicta magnam
+          reiciendis eaque? Sed, minus suscipit? Consectetur officiis ut
+          obcaecati nesciunt totam cupiditate dicta perspiciatis amet?
+        </p>{" "}
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia,
+          fugiat possimus fuga delectus ut tempora, minus nobis dolorem est
+          cumque repudiandae, numquam iure veritatis provident officiis
+          assumenda tempore quae eaque? Molestias nisi asperiores dicta magnam
+          reiciendis eaque? Sed, minus suscipit? Consectetur officiis ut
+          obcaecati nesciunt totam cupiditate dicta perspiciatis amet?
+        </p>
+      </div>
     </div>
   );
 };
