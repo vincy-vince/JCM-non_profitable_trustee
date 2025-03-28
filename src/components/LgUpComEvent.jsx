64853a9999fe20e1eaeea052c2855e15f8127c01
vincy-vince <vincyvince510@gmail.com>
2025-03-28 23:39:42 +0530

    gn

--- src/components/LgUpComEvent.jsx
+++ src/components/LgUpComEvent.jsx
@@ -8,7 +8,7 @@ const LgUpComEvent = () => {
           Upcoming events
         </h1>
         <div className="flex-row mx-20 gap-10 flex justify-around w-auto xl:mx-80">
-          <div className="bg-red-500/85 h-72 p-3 text-white rounded-lg flex flex-col justify-between w-[70%] mx-auto lg:w-[30%] md:w-[50%] xl:w-[23%]">
+          <div className="bg-red-500/85 h-72 p-3 text-white rounded-lg flex flex-col justify-between w-[70%] mx-auto lg:w-[30%] md:w-[50%] xl:w-[40%]">
             <div className="flex flex-col items-center justify-center">
               <div className="bg-white w-[100%] h-30 border-none rounded-lg"></div>
               <div className="text-lg font-bold mt-5">Womens Meeting</div>
@@ -21,7 +21,7 @@ const LgUpComEvent = () => {
             </div>
           </div>
 
-          <div className="bg-red-500/85 h-72 p-3 text-white rounded-lg flex flex-col justify-between w-[70%] mx-auto lg:w-[30%] md:w-[50%] xl:w-[23%]">
+          <div className="bg-red-500/85 h-72 p-3 text-white rounded-lg flex flex-col justify-between w-[70%] mx-auto lg:w-[30%] md:w-[50%] xl:w-[40%]">
             <div className="flex flex-col items-center justify-center">
               <div className="bg-white w-[100%] h-30 border-none rounded-lg"></div>
               <div className="text-lg font-bold mt-5">Womens Meeting</div>
@@ -34,7 +34,7 @@ const LgUpComEvent = () => {
             </div>
           </div>
 
-          <div className="bg-red-500/85 h-72 p-3 text-white rounded-lg flex flex-col justify-between w-[70%] mx-auto lg:w-[30%] md:w-[50%] xl:w-[23%]">
+          <div className="bg-red-500/85 h-72 p-3 text-white rounded-lg flex flex-col justify-between w-[70%] mx-auto lg:w-[30%] md:w-[50%] xl:w-[40%]">
             <div className="flex flex-col items-center justify-center">
               <div className="bg-white w-[100%] h-30 border-none rounded-lg"></div>
               <div className="text-lg font-bold mt-5">Womens Meeting</div>
